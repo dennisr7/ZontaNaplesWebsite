3e7e428b8bfd5ff00786ea68c8e363569d4adefd
jdmfgcu <jdmartin8609@eagle.fgcu.edu>
2025-10-30 10:55:12 -0400

    Added links to the sponsor icons for their relative websites

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -7,37 +7,79 @@ export default function Sponsor() {
       </h3>
 
       <div className="flex flex-wrap justify-center items-center gap-10 px-6">
-        <img
-          src="/src/assets/sponsor1.png"
-          alt="Sponsor 1"
-          className="w-48 h-auto hover:scale-105 transition-transform duration-300"
-        />
-        <img
-          src="/src/assets/sponsor2.png"
-          alt="Sponsor 2"
-          className="w-48 h-auto hover:scale-105 transition-transform duration-300"
-        />
-        <img
-          src="/src/assets/sponsor3.png"
-          alt="Sponsor 3"
-          className="w-48 h-auto hover:scale-105 transition-transform duration-300"
-        />
-        <img
-          src="/src/assets/sponsor4.png"
-          alt="Sponsor 4"
-          className="w-48 h-auto hover:scale-105 transition-transform duration-300"
-        />
-        <img
-          src="/src/assets/sponsor5.png"
-          alt="Sponsor 5"
-          className="w-48 h-auto hover:scale-105 transition-transform duration-300"
-        />
-        <img
-          src="/src/assets/sponsor6.png"
-          alt="Sponsor 6"
-          className="w-48 h-auto hover:scale-105 transition-transform duration-300"
-        />
+        <a
+          href="https://www.cancer.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/src/assets/sponsor1.png"
+            alt="American Cancer Society"
+            className="w-48 h-auto hover:scale-105 transition-transform duration-300"
+          />
+        </a>
+
+        <a
+          href="https://www.habitat.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/src/assets/sponsor2.png"
+            alt="Habitat for Humanity"
+            className="w-48 h-auto hover:scale-105 transition-transform duration-300"
+          />
+        </a>
+
+        <a
+          href="https://naplesshelter.org/front/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/src/assets/sponsor3.png"
+            alt="Naples Shelter"
+            className="w-48 h-auto hover:scale-105 transition-transform duration-300"
+          />
+        </a>
+
+        <a
+          href="https://path2freedom.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/src/assets/sponsor4.png"
+            alt="Path 2 Freedom"
+            className="w-48 h-auto hover:scale-105 transition-transform duration-300"
+          />
+        </a>
+
+        <a
+          href="https://projecthelpnaples.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/src/assets/sponsor5.png"
+            alt="Project Help Naples"
+            className="w-48 h-auto hover:scale-105 transition-transform duration-300"
+          />
+        </a>
+
+        <a
+          href="https://www.pacecenter.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/src/assets/sponsor6.png"
+            alt="PACE Center for Girls"
+            className="w-48 h-auto hover:scale-105 transition-transform duration-300"
+          />
+        </a>
       </div>
     </section>
   );
 }
+
